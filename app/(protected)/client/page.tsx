cae5f8dc24907aafef70d46ad8bfa4a973be3a2a
Ahmed Yasser <ahmedyasser3033@gmail.com>
2024-07-27 12:00:16 +0200

    fix

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -4,11 +4,11 @@ import UserInfo from '@/components/shared/UserInfo'
 import { useCurrentUser } from '@/hooks/user-current-user'
 import React from 'react'
 
-const clientPage = () => {
+const ClientPage = () => {
     const user =  useCurrentUser()
   return (
         <UserInfo user={user} label={"Client Component "}  />
     )
 }
 
-export default clientPage
+export default ClientPage
